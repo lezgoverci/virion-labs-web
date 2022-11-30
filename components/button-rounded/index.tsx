9d1f944c5b0725be7d22b03a718ffaac0532e3b6
Verci <vercilliusjrmila.dev@gmail.com>
2022-11-30 21:30:23 +0800

    header width mobile small

---
+++ components/button-rounded/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export const RoundedButton = ({text}:{text:string}) =>{
+    return(
+        <div className="rounded-3xl bg-gray-700 px-3 py-1 text-sm">
+            <Link href="/" title={text} >
+            {text}
+            </Link>
+        </div>
+    )
+}
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,54 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import { RoundedButton } from "../button-rounded";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
-export default function Header(){
-    return <div>
-        <Link href="/">Home</Link>
-        <Link href="/vision">Vision</Link>
-        <Link href="/ecosystem">Ecosystem</Link>
-        <Link href="/why-virion">Why Virion</Link>
-        <Link href="/partners">Partners</Link>
-        <Link href="/virion-connect">Virion Connect</Link>
+export default function Header() {
+  return (
+    <div className="py-4 bg-black text-white flex-row flex items-center px-3">
+      <div>
+        <Bars3Icon className="w-6 h-6 text-white" />
+      </div>
+      <div className="flex-1">
+        <div className="hidden self-start">
+          <Image src={"/logo.png"} width={"100"} height={"100"} />
+        </div>
+
+        <span className="pl-3">Virion Labs</span>
+      </div>
+      <div className="hidden links flex flex-row list-none">
+        {[
+          {
+            title: "Home",
+            link: "/",
+          },
+          {
+            title: "Vision",
+            link: "/",
+          },
+          {
+            title: "Ecosystem",
+            link: "/",
+          },
+          {
+            title: "Why Virion",
+            link: "/",
+          },
+          {
+            title: "Partners",
+            link: "/",
+          },
+        ].map((item) => {
+          return (
+            <li className="mx-6">
+              <Link href={item.link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </div>
+      <div className="float-right">
+        <RoundedButton text="Connect" />
+      </div>
     </div>
+  );
 }
