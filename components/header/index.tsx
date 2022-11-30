b5cd538f9a66fd08c7d826296ad196a805e067d8
hn-verci <vercillius.mila@au.harveynorman.com>
2022-11-30 12:25:47 +0800

    adds header links

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 
 export default function Header(){
     return <div>
-        {/* <Link href="/">Home</Link>
+        <Link href="/">Home</Link>
         <Link href="/careers">Careers</Link>
         <Link href="/members">Members</Link>
         <Link href="/events">Events</Link>
         <Link href="/contact">Contact</Link>
         <Link href="/updates">Updates</Link>
-        <Link href="/about">About</Link> */}
+        <Link href="/about">About</Link>
     </div>
 }
