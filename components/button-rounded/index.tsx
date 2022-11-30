b0a130b490a272aab1095094942a9860dc78beb0
Verci <vercilliusjrmila.dev@gmail.com>
2022-11-30 22:06:57 +0800

    adds toggle header nav mobile sm

--- components/button-rounded/index.tsx
+++ components/button-rounded/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export const RoundedButton = ({text}:{text:string}) =>{
     return(
-        <div className="rounded-3xl bg-gray-700 px-3 py-1 text-sm">
+        <div className="rounded-3xl bg-pink-700 px-3 py-1 text-sm">
             <Link href="/" title={text} >
             {text}
             </Link>
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,53 +2,104 @@ import Link from "next/link";
 import Image from "next/image";
 import { RoundedButton } from "../button-rounded";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export default function Header() {
-  return (
-    <div className="py-4 bg-black text-white flex-row flex items-center px-3">
-      <div>
-        <Bars3Icon className="w-6 h-6 text-white" />
-      </div>
-      <div className="flex-1">
-        <div className="hidden self-start">
-          <Image src={"/logo.png"} width={"100"} height={"100"} />
-        </div>
+  const [isOpenNavMobile, setIsOpenNavMobile] = useState(false);
 
-        <span className="pl-3">Virion Labs</span>
+  return (
+    <div className="header flex flex-col">
+      <div className="py-4 bg-black text-white flex-row flex items-center px-3">
+        <div
+          onClick={() => setIsOpenNavMobile(!isOpenNavMobile)}
+          className="btn-menu"
+        >
+          <Bars3Icon className="w-6 h-6 text-white" />
+        </div>
+        <div className="flex-1">
+          <div className="hidden self-start">
+            <Image src={"/logo.png"} width={"100"} height={"100"} />
+          </div>
+
+          <span className="pl-3">Virion Labs</span>
+        </div>
+        <div className="hidden links flex flex-row list-none">
+          {[
+            {
+              title: "Home",
+              link: "/",
+            },
+            {
+              title: "Vision",
+              link: "/",
+            },
+            {
+              title: "Ecosystem",
+              link: "/",
+            },
+            {
+              title: "Why Virion",
+              link: "/",
+            },
+            {
+              title: "Partners",
+              link: "/",
+            },
+          ].map((item, index) => {
+            return (
+              <li key={index} className="mx-6">
+                <Link href={item.link}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+        <div className="float-right">
+          <RoundedButton text="Connect" />
+        </div>
       </div>
-      <div className="hidden links flex flex-row list-none">
-        {[
-          {
-            title: "Home",
-            link: "/",
-          },
-          {
-            title: "Vision",
-            link: "/",
-          },
-          {
-            title: "Ecosystem",
-            link: "/",
-          },
-          {
-            title: "Why Virion",
-            link: "/",
-          },
-          {
-            title: "Partners",
-            link: "/",
-          },
-        ].map((item) => {
-          return (
-            <li className="mx-6">
-              <Link href={item.link}>{item.title}</Link>
-            </li>
-          );
-        })}
-      </div>
-      <div className="float-right">
-        <RoundedButton text="Connect" />
-      </div>
+
+      {isOpenNavMobile ? (
+        <div className="links flex flex-col list-none pt-4 bg-slate-900 text-gray-600">
+          {[
+            {
+              title: "Home",
+              link: "/",
+              active: true,
+            },
+            {
+              title: "Vision",
+              link: "/",
+              active: false,
+            },
+            {
+              title: "Ecosystem",
+              link: "/",
+              active: false,
+            },
+            {
+              title: "Why Virion",
+              link: "/",
+              active: false,
+            },
+            {
+              title: "Partners",
+              link: "/",
+              active: false,
+            },
+          ].map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={`pl-4 mb-3 ${
+                  item.active ? "text-white" : "text-gray-600"
+                }`}
+              >
+                <Link href={item.link}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
