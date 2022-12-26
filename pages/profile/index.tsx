5854bf73af50222c9fb54c009d17ff34813f4146
vercillius <vercilliusjrmila.dev@gmail.com>
2022-12-26 14:23:13 +0800

    adds profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import DefaultTemplate from "../../templates/default";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <DefaultTemplate>
+        <div>This is your profile page</div>
+      </DefaultTemplate>
+    </>
+  );
+}
